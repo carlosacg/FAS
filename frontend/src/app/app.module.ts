fc93ded9aa84423d2a9fa5829afa40c697131d8a
unknown <toro.diana@correounivalle.edu.co>
2018-10-11 15:49:30 -0500

    delete components header and footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,7 @@ import { FormsModule} from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { HeaderComponent } from './components/header/header.component';
 import {app_routing} from './app.routes';
 import { AccountsComponent } from './components/accounts/accounts.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
@@ -16,9 +14,7 @@ import { BudgetComponent } from './components/budget/budget.component';
   declarations: [
     AppComponent,
     UsersComponent,
-    HeaderComponent,
     NavigationComponent,
-    FooterComponent,
     AccountsComponent,
     TransactionsComponent,
     BudgetComponent,
